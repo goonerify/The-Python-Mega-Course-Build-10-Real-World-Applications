855cd9930b453640dcd2de2d0c4290101771b00b
Leslie Enwerem <lenwerem@earthwarm.org>
2021-05-02 13:15:24 -0400

    137. Popup Windows on Map

--- Section 17 Volcano & Population Web Map with Pandas and Folium/map.py
+++ Section 17 Volcano & Population Web Map with Pandas and Folium/map.py
@@ -6,15 +6,16 @@ data=pandas.read_csv('Volcanoes.txt')
 # data.columns
 lat=list(data['LAT'])
 lon=list(data['LON'])
+elev=list(data['ELEV'])
 
 map=folium.Map(location=(lat[0], lon[0]), zoom_start=6, tiles="Stamen Terrain")
 
 fg=folium.FeatureGroup(name="Map of an interesting location")
 
 # Iterate over multiple lists with `zip`
-for lt,ln in zip(lat,lon):
+for lt,ln,el in zip(lat,lon,elev):
     coords=(lt, ln)
-    fg.add_child(folium.Marker(coords, popup="A location somewhere close to Montreal", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(coords, popup=folium.Popup(str(el)+'m', parse_html=True), icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 map.save('map.html')
