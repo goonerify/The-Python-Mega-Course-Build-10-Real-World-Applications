16ddaf638db7559b05b5e13ca427c5fcc4b045d3
Leslie Enwerem <lenwerem@earthwarm.org>
2021-05-02 15:08:51 -0400

    145. Choropleth Map

--- Section 17 Volcano & Population Web Map with Pandas and Folium/map.py
+++ Section 17 Volcano & Population Web Map with Pandas and Folium/map.py
@@ -36,7 +36,8 @@ for lt,ln,el in zip(lat,lon,elev):
     fill_color=color_maker(el), fill=True, fill_opacity=1.0, color='grey'))
     # fg.add_child(folium.Marker(coords, popup=folium.Popup(str(el)+' m', parse_html=True), icon=folium.Icon(color='green')))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 map.save('map.html')
