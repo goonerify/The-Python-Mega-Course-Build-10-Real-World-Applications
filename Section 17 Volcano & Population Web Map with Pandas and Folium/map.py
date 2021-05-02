c4c9c9599ab21fe9689bab48422c0cdf0829c169
Leslie Enwerem <lenwerem@earthwarm.org>
2021-05-02 13:15:44 -0400

    138. HTML on Popups

--- Section 17 Volcano & Population Web Map with Pandas and Folium/map.py
+++ Section 17 Volcano & Population Web Map with Pandas and Folium/map.py
@@ -12,10 +12,16 @@ map=folium.Map(location=(lat[0], lon[0]), zoom_start=6, tiles="Stamen Terrain")
 
 fg=folium.FeatureGroup(name="Map of an interesting location")
 
+html = """<h4>Volcano information:</h4>
+Height: %s m
+"""
+
 # Iterate over multiple lists with `zip`
 for lt,ln,el in zip(lat,lon,elev):
     coords=(lt, ln)
-    fg.add_child(folium.Marker(coords, popup=folium.Popup(str(el)+'m', parse_html=True), icon=folium.Icon(color='green')))
+    iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+    fg.add_child(folium.Marker(coords, popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
+    # fg.add_child(folium.Marker(coords, popup=folium.Popup(str(el)+' m', parse_html=True), icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 map.save('map.html')
