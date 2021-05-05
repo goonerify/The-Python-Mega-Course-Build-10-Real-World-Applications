66604a087f2597c7d0018117d09aba3cc8af39f0
Leslie Enwerem <ifeanyi.enwerem@gmail.com>
2021-05-05 04:33:55 -0400

    182. Connecting GUI Widgets with Callback Functions

--- Section 21 Graphical User Interfaces (GUI) with Tkinter/script.py
+++ Section 21 Graphical User Interfaces (GUI) with Tkinter/script.py
@@ -2,16 +2,21 @@ from tkinter import *
 
 window=Tk()
 
-b1=Button(window, text='Execute')
+def km_to_miles():
+    miles=float(e1_value.get())*1.6
+    t1.insert(END, miles)
+
+b1=Button(window, text='Convert KM to Miles', command=km_to_miles)
 b1.grid(row=0,column=0)
 
+e1_value=StringVar()
 # Input that can be interacted with
-e1=Entry(window)
-e1.grid(row=0,column=1)
+e1=Entry(window, textvariable=e1_value)
+e1.grid(row=0, column=1)
 
 # Text widget
-t1=Text(window,height=1,width=20)
-t1.grid(row=0,column=2)
+t1=Text(window, height=1, width=20)
+t1.grid(row=0, column=2)
 
 # This should always be at the end of the main script
 window.mainloop()
