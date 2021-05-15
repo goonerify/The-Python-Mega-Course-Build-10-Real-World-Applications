ce239e84a1bdca97da484512c3b8e421f6e46eea
Leslie Enwerem <ifeanyi.enwerem@gmail.com>
2021-05-15 03:46:49 -0400

    228. Displaying Text to the User

--- Section 26 Feel-Good Mobile App with Kivy/main.py
+++ Section 26 Feel-Good Mobile App with Kivy/main.py
@@ -1,6 +1,9 @@
 # This file should be named main.py
+import glob
 import json
+import random
 from datetime import datetime
+from pathlib import Path
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -11,6 +14,16 @@ Builder.load_file('design.kv')
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+
+    def login(self, uname, pword):
+        with open('users.json') as file:
+            users = json.load(file)
+        if uname in users and users[uname]['password'] == pword:
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_wrong.text='Wrong username or password!'
+            # print("login unsuccessful for user {} with password {}".format(uname, pword))
+
 
 class RootWidget(ScreenManager):
     pass
@@ -30,6 +43,27 @@ class SignUpScreenSuccess(Screen):
     def go_to_login(self):
         self.manager.transition.direction = 'right'
         self.manager.current = 'login_screen'
+
+class LoginScreenSuccess(Screen):
+    def logout(self):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'login_screen'
+
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob(u'./quotes/*.txt')
+        available_feelings = [Path(filename).stem for filename 
+                                in available_feelings]
+
+        if feel in available_feelings:
+            with open(f"./quotes/{feel}.txt") as file:
+                quotes = file.readlines()
+                quote = random.choice(quotes)
+                self.ids.quote.text = quote
+        else:
+            self.ids.quote.text = 'Try another feeling!'
+
+
 
 class MainApp(App):
     def build(self):
